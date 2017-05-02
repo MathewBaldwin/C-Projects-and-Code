bb3343bd7c06e0b7695824ee01cfc9709316c382
MathewBaldwin <moretwomusic@gmail.com>
2017-05-02 09:28:15 -0400

    Add files via upload

---
+++ HttpClient - Pull web pages/ConsoleApp8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call the defined method to do the function
+            loadData();
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+
+        static async void loadData()
+        {
+            string page = "http://www.short-funny.com";
+            using (HttpClient Matt = new HttpClient())
+            using (HttpResponseMessage response = await Matt.GetAsync(page))
+            using (HttpContent content = response.Content)
+            {
+                string data = await content.ReadAsStringAsync();
+                if (data !=null)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
