391f7109f410d1560d2589a1cc8e5e43fc2ed0a6
MathewBaldwin <moretwomusic@gmail.com>
2017-05-02 09:30:08 -0400

    Add files via upload

---
+++ Matrix Builder/ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter N");
+            int N = int.Parse(Console.ReadLine());
+            for (int row=1; row <=N; row++)
+            {
+                for (int col = row; col <=row + N - 1;)
+                {
+                    Console.Write(col + " ");
+                    col++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+    
+
